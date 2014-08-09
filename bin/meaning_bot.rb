9280b455cc1921777524568b4bdbc490816866e6
Michael Hoffman <m@hof.fm>
2014-08-09 13:53:09 -0400

    Refactor of bot script.

--- bin/meaning_bot.rb
+++ bin/meaning_bot.rb
@@ -1,96 +1,135 @@
 #!/usr/bin/env ruby
 
-require 'rubygems'
-require 'chatterbot/dsl'
+module MeaningBot
+  require 'rubygems'
+  require 'chatterbot/dsl'
 
-CONFIG = if File.exists?('meaning_bot.yml')
-           YAML.load_file('meaning_bot.yml')
-         else
-           ENV
-         end
+  module_function
 
-consumer_key CONFIG['consumer_key']
-consumer_secret CONFIG['consumer_secret']
-secret CONFIG['secret']
-token CONFIG['token']
 
-# remove this to send out tweets
-#debug_mode
+  # remove this to send out tweets
+  #debug_mode
 
-# remove this to update the db
-no_update
-# remove this to get less output when running
-verbose
+  # remove this to update the db
+  #no_update
 
-###
-# Helpers
-###
+  # remove this to get less output when running
+  #verbose
 
-EXCLUSIONS = %w{? " 42}.map{|e| "-#{e}"}.join(' ')
+  ###
+  # Helpers
+  ###
 
-def search_term(base, modifiers)
-  "\"#{base}\" " + EXCLUSIONS + ' ' + modifiers
-end
+  SUBJECT_SIGNATURE = ' is the meaning of life'
+  PREDICATE_SIGNATURE = 'the meaning of life is '
+  UNDESIRABLE_CHARS = /http|@|meaning/
 
-###
-# Bot Script
-###
-
-if rand(10) == 7 # Only tweet once every 100 minutes
-
-  left_tweets = []
-  search search_term('is the meaning of life', '-what') do |tweet|
-    left_tweets << tweet
+  def search_term(base, modifiers)
+    "\"#{base}\" " + '-? -42 -Christ' + modifiers
   end
 
-  right_tweets = []
-  search search_term('the meaning of life is', '-give') do |tweet|
-    right_tweets << tweet
+  def one_nth_of_the_time(n)
+    rand(n) == 0
   end
 
-  recent_tweet_text = client.user_timeline(
-    :screen_name => 'meaningbot',
-    :count => 200,
-    :trim_user => true
-  ).map(&:text).join
-
-  subject_tweet = left_tweets.map do |tweet|
-    {
-      :tweet => tweet,
-      :text => tweet.text.sub(/is the meaning of life.*/i, '').strip.delete('\""')
-    }
-  end.shuffle.find do |tweet|
-    !(tweet[:text] =~ /http|@|meaning/) &&
-    !(recent_tweet_text.index(tweet[:text]))
-  end
-
-  predicate_tweet = right_tweets.map do |tweet|
-    {
-      :tweet => tweet,
-      :text => tweet.text.sub(/.*the meaning of life is /i, '').strip.delete('\""')
-    }
-  end.shuffle.find do |tweet|
-    (tweet[:text].length + subject_tweet[:text].length) < 136 &&
-      !(tweet[:text] =~ /http|@|meaning/) &&
-      !(recent_tweet_text.index(tweet[:text]))
-  end
-
-  if subject_tweet[:text] && predicate_tweet[:text]
-    aphorism = subject_tweet[:text] + ' is ' + predicate_tweet[:text]
-    if tweet(aphorism)
-      client.favorite(subject_tweet[:tweet])
-      client.favorite(predicate_tweet[:tweet])
-      puts "Tweeted: " + aphorism
-    else
-      puts "Failed to tweet: " + aphorism
+  def get_search_tweets(query)
+    tweets = []
+    search query do |tweet|
+      tweets << tweet
     end
-  else
-    puts "Not enough data."
-    puts "LEFT: " + subject_tweet[:text].inspect
-    puts "RIGHT: " + predicate_tweet[:text].inspect
+    since_id(0)
+
+    tweets
   end
 
-else
-  puts "Staying silent this time."
+  def subject_tweets
+    get_search_tweets search_term(SUBJECT_SIGNATURE, '-what')
+  end
+
+  def predicate_tweets
+    get_search_tweets search_term(PREDICATE_SIGNATURE, '-give')
+  end
+
+  def recently_tweeted_text
+    text = client.user_timeline(
+      :screen_name => 'meaningbot',
+      :count => 200,
+      :trim_user => true
+    ).map(&:text).join.downcase
+
+    since_id(0)
+
+    text
+  end
+
+  def pair_of_tweets
+    recents = recently_tweeted_text
+
+    subject_tweet = subject_tweets.map do |tweet|
+      {
+        :tweet => tweet,
+        :snippet => tweet.text.sub(/#{SUBJECT_SIGNATURE}.*/i, '').strip.delete('\""')
+      }
+    end.shuffle.find do |tweet|
+      !(tweet[:snippet] =~ UNDESIRABLE_CHARS) &&
+        !(recents.index(tweet[:snippet].downcase))
+    end
+
+    predicate_tweet = predicate_tweets.map do |tweet|
+      {
+        :tweet => tweet,
+        :snippet => tweet.text.sub(/.*#{PREDICATE_SIGNATURE}/i, '').strip.delete('\""')
+      }
+    end.shuffle.find do |tweet|
+      (tweet[:snippet].length + subject_tweet[:snippet].length + 4) < 140 &&
+        !(tweet[:snippet] =~ UNDESIRABLE_CHARS) &&
+        !(recents.index(tweet[:snippet].downcase))
+    end
+
+    [subject_tweet, predicate_tweet]
+  end
+
+
+  def run(opts)
+    if one_nth_of_the_time(10) || opts[:force]
+      subject_tweet, predicate_tweet = pair_of_tweets
+
+      if subject_tweet && predicate_tweet
+        aphorism = subject_tweet[:snippet] + ' is ' + predicate_tweet[:snippet]
+
+        puts "*"*10
+        if opts[:testing]
+          puts "TESTING MODE. NOT TWEETING."
+        else
+          puts "TWEETING!"
+          tweet(aphorism)
+          client.favorite(subject_tweet[:tweet])
+          client.favorite(predicate_tweet[:tweet])
+        end
+        puts "SUBJECT FULL TEXT: " + subject_tweet[:tweet].text
+        puts "PREDICATE FULL TEXT: " + predicate_tweet[:tweet].text
+        puts "TWEET TEXT: " + aphorism
+        puts "*"*10
+      else
+        puts "Not enough material."
+      end
+    else
+      puts "Staying silent this time."
+    end
+  end
+
 end
 
+CREDS = if File.exists?('bin/meaning_bot.yml')
+          YAML.load_file('bin/meaning_bot.yml')
+        else
+          ENV.symbolize_keys!
+        end
+
+consumer_key CREDS[:consumer_key]
+consumer_secret CREDS[:consumer_secret]
+secret CREDS[:secret]
+token CREDS[:token]
+
+
+MeaningBot.run(:testing => true, :force => true)
